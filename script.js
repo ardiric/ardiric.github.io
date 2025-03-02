ff1329023b95efce6359e0f9c6e9e964400ad41c
Herdy01 <riccardo.ardizzoni@studio.unibo.it>
2025-03-02 22:25:51 +0100

    Mobile menu

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 document.getElementById("currentYear").textContent = new Date().getFullYear();
+
+function toggleMenu() {
+    const menu = document.getElementById("mobileMenu");
+    menu.style.display = menu.style.display === "flex" ? "none" : "flex";
+}
+
+function closeMenu() {
+    document.getElementById("mobileMenu").style.display = "none";
+}
