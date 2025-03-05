8a6961d244a75d79c1d11c98597fd0976df93c74
ardiric <114171617+ardiric@users.noreply.github.com>
2025-03-05 11:26:03 +0100

    Update formazione.js

--- pages/formazione.js
+++ pages/formazione.js
@@ -1,57 +1,14 @@
-/*
-let formazione = `
-<div class="main-content">
-
-    <div class="form-content">
-        <p class="data">yyyy - yyyy</p>
-        <div id="dot-line">
-            <span class="dot"></span>
-            <div class="line"></div>
-        </div>
-        <div>
-            <h3 class="form-des">Form des</h3>
-            <p class="altre-info">Altre info</p>
-        </div>
-    </div>
-
-    <div class="form-content">
-        <p class="data">yyyy - yyyy</p>
-        <div id="dot-line">
-            <span class="dot"></span>
-            <div class="line"></div>
-        </div>
-        <div>
-            <h3 class="form-des">Form des</h3>
-            <p class="altre-info">Altre info</p>
-        </div>
-    </div>
-
-    <div class="form-content">
-        <p class="data">yyyy - yyyy</p>
-        <div id="dot-line">
-            <span class="dot"></span>
-        </div>
-        <div>
-            <h3 class="form-des">Form des</h3>
-            <p class="altre-info">Altre info</p>
-        </div>
-    </div>
-
-</div>
-`;
-*/
-
 // Creazione dell'oggetto JSON
 const datiFormazione = [
     { 
         "date": "2024 - oggi", 
         "descrizione": "Laurea Magistrale in Informatica" ,
-        "info": "Università degli studi di Bologna.<br>Dipartimento di Informatica - Scienza e Ingegneria."
+        "info": "Università degli studi di Bologna"
     },
     { 
         "date": "2020 - 2024", 
         "descrizione": "Laurea in Informatica per il Management" ,
-        "info": "Università degli studi di Bologna.<br>Dipartimento di Informatica - Scienza e Ingegneria."
+        "info": "Università degli studi di Bologna"
     },
     { 
         "date": "2015 - 2020", 
@@ -62,7 +19,10 @@ const datiFormazione = [
 
 function generaBloccoFormazione() {
 
-    let blocks = `<div class="main-content">`;
+    let blocks = `
+        <div class="main-content">
+            <h2 class="main-content-title">Formazione</h2>
+    `;
     
     datiFormazione.forEach((item, index) => {
 
@@ -75,8 +35,10 @@ function generaBloccoFormazione() {
                 <span class="dot"></span>
                 ${isLastItem ? '' : '<div class="line"></div>'}
             </div>
-            <h3 class="form-des">${item.descrizione}</h3>
-            <p class="altre-info">${item.info}</p>
+            <div>
+                <h3 class="form-des">${item.descrizione}</h3>
+                <p class="altre-info">${item.info}</p>
+            </div>
         </div>
         `;
 
