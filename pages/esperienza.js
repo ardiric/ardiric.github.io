13a470c659f13961927954931b08bdb40364b37e
ardiric <114171617+ardiric@users.noreply.github.com>
2025-03-05 11:25:14 +0100

    Update esperienza.js

--- pages/esperienza.js
+++ pages/esperienza.js
@@ -1,63 +1,23 @@
-/*
-let esperienza = `
-<div class="main-content">
-
-    <div class="esp-content">
-        <p class="data">yyyy - yyyy</p>
-        <div id="dot-line">
-            <span class="dot"></span>
-            <div class="line"></div>
-        </div>
-        <div>
-            <h3 class="esp-des">Esp des</h3>
-            <p class="altre-info">Altre info</p>
-        </div>
-    </div>
-
-    <div class="esp-content">
-        <p class="data">yyyy - yyyy</p>
-        <div id="dot-line">
-            <span class="dot"></span>
-            <div class="line"></div>
-        </div>
-        <div>
-            <h3 class="esp-des">Esp des</h3>
-            <p class="altre-info">Altre info</p>
-        </div>
-    </div>
-
-    <div class="esp-content">
-        <p class="data">yyyy - yyyy</p>
-        <div id="dot-line">
-            <span class="dot"></span>
-        </div>
-        <div>
-            <h3 class="esp-des">Esp des</h3>
-            <p class="altre-info">Altre info</p>
-        </div>
-    </div>
-
-</div>
-`;
-*/
-
 // Creazione dell'oggetto JSON
 const datiEsperienza = [
     { 
         "date": "ott 2023 - nov 2023<br>gen 2023 - mar 2023", 
-        "descrizione": "Sviluppatore Web (tirocinio) presso Centro Software S.p.A." ,
-        "info": "Mansioni:<br>- sviluppo sito web<br>- creazione applicazione web per Smart TV e del relativo backend"
+        "descrizione": "Sviluppatore Web (tirocinio)" ,
+        "info": "Centro Software S.p.A."
     },
     { 
         "date": "mag 2019 - giu 2019<br>gen 2018 - feb 2018", 
-        "descrizione": "Impiegato ufficio mutui (stage) presso Cassa di Risparmio di Cento" ,
-        "info": "Mansioni:<br>- inserimento dati relativi ai mutui nel sistema interno<br>- rapporti con i notai per richiedere copie digitali dei contratti"
+        "descrizione": "Impiegato ufficio mutui (stage)" ,
+        "info": "Cassa di Risparmio di Cento"
     }
 ];
 
 function generaBloccoEsperienza() {
 
-    let blocks = `<div class="main-content">`;
+    let blocks = `
+        <div class="main-content">
+            <h2 class="main-content-title">Esperienza lavorativa</h2>
+    `;
     
     datiEsperienza.forEach((item, index) => {
 
@@ -70,8 +30,10 @@ function generaBloccoEsperienza() {
                 <span class="dot"></span>
                 ${isLastItem ? '' : '<div class="line"></div>'}
             </div>
-            <h3 class="esp-des">${item.descrizione}</h3>
-            <p class="altre-info">${item.info}</p>
+            <div>
+                <h3 class="esp-des">${item.descrizione}</h3>
+                <p class="altre-info">${item.info}</p>
+            </div>
         </div>
         `;
 
