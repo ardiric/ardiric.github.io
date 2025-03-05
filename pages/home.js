3bb18bf2de3a4561a28288d90fb2d91842791c2c
ardiric <114171617+ardiric@users.noreply.github.com>
2025-03-05 11:24:43 +0100

    Update home.js

--- pages/home.js
+++ pages/home.js
@@ -1,5 +1,5 @@
 let home = `
-<div class="main-content">
+<div class="main-content home-page-content">
     <h1><span>Riccardo Ardizzoni</span><br>Software Developer</h1>
     <p class="margin-bottom">Lorem ipsum dolor sit amet consectetur adipiscing elit.</p>
     <button class="btn black" onclick="loadProgetti()">Progetti</button>
