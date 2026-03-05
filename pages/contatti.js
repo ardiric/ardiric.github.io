25f7343a1acc04bc4987ff7913cb7bd02c4ff21c
Herdy01 <riccardo.ardizzoni@studio.unibo.it>
2026-03-05 19:24:11 +0100

    Update

--- pages/contatti.js
+++
@@ -1,7 +0,0 @@
-let contatti = `
-<div class="main-content">
-
-    <p>Contatti - Work in progress</p>
-
-</div>
-`;
--- pages/home.js
+++
@@ -1,8 +0,0 @@
-let home = `
-<div class="main-content home-page-content">
-    <h1><span>Riccardo Ardizzoni</span><br>Software Developer</h1>
-    <!--<p class="margin-bottom">Lorem ipsum dolor sit amet consectetur adipiscing elit.</p>-->
-    <button class="btn black" onclick="loadProgetti()">Progetti</button>
-    <a href="https://www.linkedin.com/in/riccardo-ardizzoni-427066265/" class="btn orange">LinkedIn</a>
-</div>
-`;
